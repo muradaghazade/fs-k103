5b9f88bb014bb77b8122d2478013fdbf12544ec4
muradaghazade <xomurad41@gmail.com>
2025-02-04 17:32:18 +0400

    m

--- core/models.py
+++ core/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+#Recipe, User, Story Comment Category, Tag, Contact, Subscriber
+
+class Story(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='files/')
+    text = models.TextField()
+    views = models.IntegerField(default=0)
+    # category
+    # tag
+    # comment
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='files/')
+    text = models.TextField()
+    views = models.IntegerField(default=0)
+    # category
+    # tag
+    # comment
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    # user
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='files/')
+
+    def __str__(self):
+        return self.title
+    
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Subscriber(models.Model):
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
