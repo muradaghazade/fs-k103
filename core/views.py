94bc4b3705ee0c19fe35311a2a0c27d72f73ddda
muradaghazade <xomurad41@gmail.com>
2025-02-11 17:28:00 +0400

    m

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from core.models import Story
+
 
 def home(request):
-    return render(request, 'index.html')
+    stories = Story.objects.order_by('-id')
+    context = {
+        'stories': stories
+    }
+    return render(request, 'index.html', context)
