3f0f9ecfdc253ba83403304ca85a4b231a98f000
muradaghazade <xomurad41@gmail.com>
2025-02-13 16:38:24 +0400

    m

--- core/views.py
+++ core/views.py
@@ -6,9 +6,11 @@ def home(request):
     stories = Story.objects.order_by('-id')
     categories = Category.objects.all()
     new_story = stories.first()
+    holiday_stories = stories.filter(category__title='Holiday').order_by('-id')
     context = {
         'stories': stories,
         'new_story': new_story,
-        'categories': categories
+        'categories': categories,
+        'holiday_stories': holiday_stories
     }
     return render(request, 'index.html', context)
